d0d80085661b857b57823c81e6c31000ba40ccec
akaCube <dennnis1995@live.de>
2016-04-06 15:15:15 +0200

    added a google font, the logo now floats over the navbar, added simple colors

--- wp-content/themes/ziz-theme/templates/header.php
+++ wp-content/themes/ziz-theme/templates/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-fixed-top" style="background-color:#080808">
+<nav class="navbar navbar-top" style="background-color:#080808; border-radius:0px; border-bottom: 3px solid #fff; font-size: 1.2rem">
   <div class="container">
     <div class="row">
       <div class="col-md-5">
@@ -17,10 +17,10 @@
           </ul>
         </nav>
       </div>
-      <div class="col-md-2">
-        <img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" class="img-fluid" style="position:absolute; height: 10rem; border: 3px solid #fff" />
+      <div class="col-md-2" style="padding:0">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" class="img-fluid" style="position:absolute; border: 3px solid #fff" />
       </div>
-      <div class="col-md-5">
+      <div class="col-md-5 pull-right">
         <nav class="navbar navbar-dark">
           <ul class="nav navbar-nav pull-sm-left">
             <li class="nav-item <?php if(is_page('guides')){ echo "active"; }?>">
@@ -38,3 +38,39 @@
     </div>
   </div>
 </nav>
+
+<!-- Alternative Header 
+<nav class="navbar navbar-top" style="background-color:#080808; border-radius:0px; border-bottom: 3px solid #fff; font-size: 1.2rem">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2" style="padding:0">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" class="img-fluid" style="position:absolute; border: 3px solid #fff" />
+      </div>
+      <div class="col-md-10">
+        <nav class="navbar navbar-dark">
+          <ul class="nav navbar-nav pull-sm-left">
+            <li class="nav-item <?php if(is_front_page()){ echo "active"; }?>">
+              <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item <?php if(is_page('toolkit')){ echo "active"; }?>">
+              <a class="nav-link" href="/toolkit/">Ziz's Toolkit</a>
+            </li>
+            <li class="nav-item <?php if(is_page('builds')){ echo "active"; }?>">
+              <a class="nav-link" href="/builds/">Builds</a>
+            </li>
+            <li class="nav-item <?php if(is_page('guides')){ echo "active"; }?>">
+              <a class="nav-link" href="/guides/">Guides</a>
+            </li>
+            <li class="nav-item <?php if(is_page('stream')){ echo "active"; }?>">
+              <a class="nav-link" href="/stream/">Steam</a>
+            </li>
+            <li class="nav-item <?php if(is_page('about')){ echo "active"; }?>">
+              <a class="nav-link" href="/about/">About</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</nav>
+--!>
