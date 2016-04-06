408c84896dbb45e3e00eec4921541a74453bd926
akaCube <dennnis1995@live.de>
2016-04-06 14:29:07 +0200

    Changed the header, added a logo, added the pages to fit the nav

--- wp-content/themes/ziz-theme/templates/header.php
+++ wp-content/themes/ziz-theme/templates/header.php
@@ -1,12 +1,40 @@
-<header class="banner">
+<nav class="navbar navbar-fixed-top" style="background-color:#080808">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <div class="row">
+      <div class="col-md-5">
+        <nav class="navbar navbar-dark">
+          <ul class="nav navbar-nav pull-sm-right">
+            <li class="nav-item <?php if(is_front_page()){ echo "active"; }?>">
+              <a class="nav-link" href="/">Home</a>
+            </li>
+            <li class="nav-item <?php if(is_page('toolkit')){ echo "active"; }?>">
+              <a class="nav-link" href="/toolkit/">Ziz's Toolkit</a>
+            </li>
+            <li class="nav-item <?php if(is_page('builds')){ echo "active"; }?>">
+              <a class="nav-link" href="/builds/">Builds</a>
+            </li>
+
+          </ul>
+        </nav>
+      </div>
+      <div class="col-md-2">
+        <img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" class="img-fluid" style="position:absolute; height: 10rem; border: 3px solid #fff" />
+      </div>
+      <div class="col-md-5">
+        <nav class="navbar navbar-dark">
+          <ul class="nav navbar-nav pull-sm-left">
+            <li class="nav-item <?php if(is_page('guides')){ echo "active"; }?>">
+              <a class="nav-link" href="/guides/">Guides</a>
+            </li>
+            <li class="nav-item <?php if(is_page('stream')){ echo "active"; }?>">
+              <a class="nav-link" href="/stream/">Steam</a>
+            </li>
+            <li class="nav-item <?php if(is_page('about')){ echo "active"; }?>">
+              <a class="nav-link" href="/about/">About</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </div>
-</header>
+</nav>
